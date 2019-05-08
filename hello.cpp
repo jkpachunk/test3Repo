95b752c1a5bf104bf217a4a79031e06d3dc48f55
Jon Pachunka <jkpachunka@unomaha.edu>
2019-05-08 14:39:31 -0500

    First commit of the repo

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+// Example cpp program
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num1;
+	cout << "Hello there!\n" << "Type something here: ";	// Prints the strings to the terminal.
+	cin >> num1;											// Takes an integer input from the user.
+	cout << "THE NUMBER IS: " << num1 << "\n";				// Prints the integer input from the user.
+
+	return 0;
+}
