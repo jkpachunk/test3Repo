017ffeb37f2b856ee7985d37023b16b1d44f5e5a
Jon Pachunka <jkpachunka@unomaha.edu>
2019-05-08 15:29:51 -0500

    Additional program lines

--- hello.cpp
+++ hello.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	int num1;
 	cout << "Hello there!\n" << "Type something here: ";	// Prints the strings to the terminal.
-	cin >> num1;											// Takes an integer input from the user.
-
+	cin >> num1;										// Takes an integer input from the user.
+	cout << "THE NUMBER IS: " << num1 << "\n";
 	return 0;
 }
